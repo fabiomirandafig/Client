035e4a258ec9ba2189a9aa956c73d04a56938052
Fábio <fabiomirandafig@gmail.com>
2021-11-10 22:47:12 -0300

    NuxtJS Crash Course

---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <Hero />
+    <LargeCardDisplay
+      v-for="cardInfo in largeCardInfo"
+      :key="cardInfo.id"
+      :cards-section="cardInfo"
+    />
+    <SmallCardDisplay
+      v-for="cardInfo in smallCardSections"
+      :key="cardInfo.id"
+      :cards-section="cardInfo"
+    />
+  </div>
+</template>
+
+<script>
+// import Hero from '../components/Hero.vue'
+// import LargeCardDisplay from '../components/LargeCardDisplay.vue'
+// import SmallCardDisplay from '../components/SmallCardDisplay.vue'
+import { largeCardSections, smallCardSections } from '@/assets/data.js'
+export default {
+  // components: {
+  //   Hero,
+  //   LargeCardDisplay
+  // },
+  data () {
+    return {
+      largeCardInfo: largeCardSections,
+      smallCardSections
+    }
+  }
+}
+</script>
